9611294bbbe27f8c97ae797dcdcf3fbd3415f760
Tejaswini Nayak <tnayak@endor.ai>
2024-12-18 12:08:20 +0530

    change sast rule

--- sast-testing/vulnerable_script.py
+++ sast-testing/vulnerable_script.py
@@ -1,11 +1,22 @@
-# src/python/vulnerable_script.py
-import os
+import cPickle  # Importing the cPickle module
 import sys
 
-def execute_command(user_input):
-    os.system(f"ping {user_input}")
+def deserialize(data):
+    try:
+        # Deserialize the data
+        deserialized_data = cPickle.loads(data)
+        print("Deserialization successful:", deserialized_data)
+    except Exception as e:
+        print("Error during deserialization:", e)
 
 if __name__ == "__main__":
+    # Simulating untrusted data from an untrusted source (e.g., a web request)
     user_input = sys.argv[1]
-    execute_command(user_input)
+
+    # In a real-world scenario, this data should be sanitized and validated
+    try:
+        # Attempt to deserialize the input data
+        deserialize(user_input)
+    except Exception as e:
+        print("Failed to deserialize data:", e)
 
